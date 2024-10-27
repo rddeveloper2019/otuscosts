f3d19a9d7b2fdddf8aedd8eb225159cdf500a481
Rid Van <fatzey2010@gmail.com>
2024-10-27 11:31:52 +0300

    chore(*): add sass compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import sass from 'sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
