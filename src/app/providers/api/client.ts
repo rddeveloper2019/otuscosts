0ab7b8b7b5cb1f7311633caeb59c283d1115c2f2
Rid Van <fatzey2010@gmail.com>
2024-11-02 00:17:38 +0300

    fix(client): apollo token

--- src/app/providers/api/client.ts
+++ src/app/providers/api/client.ts
@@ -1,14 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { commandId, URL } from '@/app/providers/api/constants/client.ts';
 import { TokenService } from '@/shared/services/TokenService.ts';
+import { setContext } from '@apollo/client/link/context';
 
 const tokenService = TokenService.getInstance(commandId);
 
-console.log('(**)=> tokenService.getToken(): ', tokenService.getToken());
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: URL,
-  cache: new InMemoryCache({ resultCaching: false }),
-  headers: {
-    authorization: `Bearer ${tokenService.getToken()}`,
-  },
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = tokenService.getToken();
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
